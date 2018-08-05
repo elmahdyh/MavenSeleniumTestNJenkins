003d383e2b83b0b7e32a9f0c0ca8412ad4193ed9
elmahdyh <31018801+elmahdyh@users.noreply.github.com>
2018-08-05 05:10:41 +0200

    Update NewTest.java

--- src/main/java/tests/NewTest.java
+++ src/main/java/tests/NewTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
